7387a009309e0e3538c929c8214dfe6573181b03
陈泵 <mscb402@gmail.com>
2019-03-03 16:06:57 +0800

    GraphLayer interface

---
+++ GraphLayer.js
@@ -0,0 +1,23 @@
+class GraphLayer{
+    constructor(p,gd){
+        this.LayerName = "";
+        this.p = p;
+        this.gd = gd;
+        this.currentStatus = "Nomal";
+    }
+    UpdateStatus(){
+        return null; //or GraphLayerOption
+    }
+    IsPointAtLayer(p){
+        return false;
+    }
+    NomalDraw(){
+        return true;
+    }
+    ActiveDraw(){
+        return true;
+    }
+    FoucsDraw(){
+        return true;
+    }
+}
