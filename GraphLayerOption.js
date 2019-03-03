a47506152294d70be36325d24ffcb3eb9ef0483e
陈泵 <mscb402@gmail.com>
2019-03-03 16:08:32 +0800

    GraphLayerOption interface

---
+++ GraphLayerOption.js
@@ -0,0 +1,13 @@
+class GraphLayerOption{
+    constructor(){
+        this.Option = {};
+    }
+
+    GetOptionList(){
+        return this.Option;
+    }
+
+    SetOptionList(Option){
+        return true;
+    }
+}
