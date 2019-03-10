0fc3b26a7672e91887c6c68dbcf77d5b911e7fd4
陈泵 <mscb402@gmail.com>
2019-03-10 13:40:07 +0800

    添加测试案例

--- index.js
+++ index.js
@@ -13,11 +13,13 @@ let s = gd.getCurrentStyle();
 s.strokeStyle = "#0000ff"; //修改画笔颜色
 gd.SetStyle(s);//刷新画布样式
 gd.DrawRect(new Point(70,60),50,60)//画图
-console.log(gd.usedStyle);
+//console.log(gd.usedStyle);
 gd.InitStyle();//恢复样式
-console.log(gd.usedStyle);
+//console.log(gd.usedStyle);
 gd.DrawPoint(new Point(30,78),10);
 
 gd.DrawTriangle(new Point(100,30),
                 new Point(200,90),
                 new Point(70,100))
+
+gd.DrawText(new Point(140,110),"ChenBeng")
