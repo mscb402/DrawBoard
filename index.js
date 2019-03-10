3f8a6542daf3f5f2d7dea6171579e8353cbeda5d
陈泵 <mscb402@gmail.com>
2019-03-10 13:32:50 +0800

    增加barrier例子

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let can = document.getElementById("ctx");
 let ctx = can.getContext('2d');
 
 let gd = new GraphDrawer(ctx,new Point(0,0));
+gd.barrier = new Point(-40,-10);//
 gd.DrawLine(new Point(10,10),new Point(50,50));
 gd.DrawPoint(new Point(60,60),5);
 //获取当前样式
