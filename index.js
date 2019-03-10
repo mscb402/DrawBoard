ec1c24b84e7f20e34b5f2cc07ce703fe4b4b9611
陈泵 <mscb402@gmail.com>
2019-03-10 12:08:36 +0800

    添加测试 DrawLine

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import Point from "./Point.js"
+import {GraphDrawer} from "./GraphDrawer.js"
+import Point from "./Point.js";
+let can = document.getElementById("ctx");
+let ctx = can.getContext('2d');
 
-let p = new Point(1,2);
-console.log(p);
+let gd = new GraphDrawer(ctx,new Point(0,0));
+gd.DrawLine(new Point(10,10),new Point(50,50));
+//console.log(p);
