baf4430397c345aa4c5ab7d2276a989686a7f53f
陈泵 <mscb402@gmail.com>
2019-03-10 11:40:35 +0800

    设置屏障初始值为左上角

--- GraphDrawer.js
+++ GraphDrawer.js
@@ -1,6 +1,14 @@
+import Point from "Point.js";
 class GraphDrawer{
     constructor(ctx,barrier){
+        //barrier是屏障
+        //用于计算其他组件的相对位置
+        //默认的barrier应该为（0，0），也就是左上角
         this.barrier = barrier;
+        if(barrier == null){
+            this.barrier = new Point(0,0);
+        }
+        
         this.ctx = ctx;
         this.defaultStyle = new DrawStyle();
         this.usedStyle = this.defaultStyle;
