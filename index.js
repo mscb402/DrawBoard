c49e7c38fd8b4ba01b24e2b7da653b1bae8d2417
陈泵 <mscb402@gmail.com>
2019-03-10 13:26:47 +0800

    测试

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ let gd = new GraphDrawer(ctx,new Point(0,0));
 gd.DrawLine(new Point(10,10),new Point(50,50));
 gd.DrawPoint(new Point(60,60),5);
 //获取当前样式
-let Style = gd.getCurrentStyle();
-Style.strokeStyle = "#0000ff"; //修改画笔颜色
-gd.SetStyle(Style);//刷新画布样式
+let s = gd.getCurrentStyle();
+s.strokeStyle = "#0000ff"; //修改画笔颜色
+gd.SetStyle(s);//刷新画布样式
 gd.DrawRect(new Point(70,60),50,60)//画图
-
+console.log(gd.usedStyle);
 gd.InitStyle();//恢复样式
-
+console.log(gd.usedStyle);
 gd.DrawPoint(new Point(30,78),10);
+
+gd.DrawTriangle(new Point(100,30),
+                new Point(200,90),
+                new Point(70,100))
