4a3bcd8ad4ca6f0e039e0e6b615e5a389195d7d8
陈泵 <mscb402@gmail.com>
2019-03-18 14:47:00 +0800

    实现渲染方法

--- Render.js
+++ Render.js
@@ -6,7 +6,23 @@ class Render{
         return this.layers.push(gl) >= 0;
     }
     Render(){
-
+        //遍历每一个层，并根据状态渲染
+        this.layers.forEach(e =>{
+            switch (e.currentStatus) {
+                case "Nomal":
+                    e.NormalDraw();
+                    break;
+                case "Focus":
+                    e.FocusDraw();
+                    break;
+                case "Active":
+                    e.ActiveDraw();
+                    break;
+                default:
+                    //不做操作
+                    break;
+            }
+        });
     }
 }
 export {Render};
