f78a6cacb68f0caabb1690136e7173441dae1aa6
陈泵 <mscb402@gmail.com>
2019-03-19 17:26:24 +0800

    update

--- CONST.js
+++ CONST.js
@@ -2,5 +2,10 @@
 export let C = {
     NORMAL_STATUS : "Normal",
     ACTIVE_STATUS : "Active",
-    FOCUS_STATUS : "Focus"
+    FOCUS_STATUS : "Focus",
+
+    MOUSE_OVER : "mouse_over",
+    MOUSE_DOWN : "mouse_down",
+    MOUSE_UP   : "mouse_up",
+    MOUSE_NONE : "mouse_none"
 }
--- Interactive.js
+++ Interactive.js
@@ -1,4 +1,5 @@
 import {Point} from "./Point";
+import {C} from "./CONST"
 class Interactive{
     constructor(canv,render){
         this.canv = canv;
@@ -12,7 +13,15 @@ class Interactive{
     }
     mousemove(e){
         let pos = this.getMousePos(e);
-        console.log(pos);
+        //console.log(pos);
+        let ls = this.getWasPointedLayers(pos);
+        /*
+        暂时先不处理
+        ls.forEach(el => {
+            el.currentStatus = C.FOCUS_STATUS;
+            el.UpdateStatus();
+        });
+        */
         this.render.Render();
     }
     mousedown(e){
@@ -21,7 +30,38 @@ class Interactive{
     mouseup(e){
 
     }
-
+    getWasPointedLayers(p){
+        let ls = [];
+        for(let i=0;i<this.render.layers.length;i++){
+            let l = this.render.layers[i];
+            if(l.IsPointAtLayer(p)){
+                ls.push(l);
+            }
+        }
+        return ls;
+    }
+    //这个函数最后写
+    statusTrans(layerSts,currentMouseSts){
+        switch (layerSts) {
+            case C.NORMAL_STATUS:
+                if(currentMouseSts == C.MOUSE_OVER){
+                    return C.FOCUS_STATUS;
+                }
+                break;
+            case C.FOCUS_STATUS:
+                if(currentMouseSts == C.MOUSE_DOWN){
+                    return C.ACTIVE_STATUS;
+                }
+                if(currentMouseSts == C.MOUSE_OVER){
+                    return C.FOCUS_STATUS;
+                }
+            case C.ACTIVE_STATUS:
+                break;
+            default:
+                return C.NORMAL_STATUS;
+        }
+        
+    }
     getMousePos(evt) {
         var rect = this.canv.getBoundingClientRect();
         return new Point(
