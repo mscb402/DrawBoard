da8bf05e451ff50a3aa594bc86099803001d85e3
陈泵 <mscb402@gmail.com>
2019-04-03 16:10:52 +0800

    add case

--- index.js
+++ index.js
@@ -15,10 +15,11 @@ let render = new Render();
 //let test = new arcRectWithText(new Point(10,10),gd,100,100,"hello");
 //let test2 = new arcRectWithText(new Point(150,100),gd,100,100,"hello");
 let box = new BoxLayer(new Point(300,10),gd,50,100,10,"Test");
-let line = new LinePointTo(new Point(300+50,10+50),gd,new Point(500+50,10+50))
+let line = new LinePointTo(new Point(300+50 + 2,10+50),gd,new Point(500 - 2,10+50),"10X20")
 let box2 = new BoxLayer(new Point(500,10),gd,50,100,10,"Test2");
 render.AddLayer(box);
-render.AddLayer(line);
+
 render.AddLayer(box2);
+render.AddLayer(line);
 let itac = new Interactive(can,render,gd);
 itac.start();//启动交互
